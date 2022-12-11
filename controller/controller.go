2794d9cc789d903f1bc5ad0863d7c8ab9af3b7f9
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-12-11 16:42:34 +0900

    create

--- controller/controller.go
+++ controller/controller.go
@@ -1,15 +1,51 @@
 package controller
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 type TodoController struct {}
 
+type TodoItem struct {
+	Id uuid.UUID `json:"id"`
+	Title string `json:"title"`
+	Content string `json:"content"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type TodoRequest struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+var todoList []TodoItem
+
 func (tc TodoController) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("作成")
+
+	body := make([]byte, r.ContentLength)
+	r.Body.Read(body)
+	var todoRequest TodoRequest
+	json.Unmarshal(body, &todoRequest)
+
+	fmt.Printf("CREATE")
+	fmt.Printf(todoRequest.Title)
+
+	todoList = append(todoList, TodoItem{
+		Id: uuid.New(),
+		Title: todoRequest.Title,
+		Content: todoRequest.Content,
+		CreatedAt: time.Now(),
+	})
+
 	fmt.Fprintf(w, "作成")
+
 }
 
 func (tc TodoController) GetById(w http.ResponseWriter, r *http.Request) {
@@ -18,8 +54,15 @@ func (tc TodoController) GetById(w http.ResponseWriter, r *http.Request) {
 }
 
 func (tc TodoController) GetAll(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("全件取得")
-	fmt.Fprintf(w, "全件取得")
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(&todoList); err != nil {
+		log.Fatal(err)
+	}
+	_, err := fmt.Fprint(w, buf.String())
+	if err != nil {
+		return
+	}
 }
 
 func (tc TodoController) Update(w http.ResponseWriter, r *http.Request) {
