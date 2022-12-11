69aba236d4aeb6f13512da863f7fb031099037b0
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-12-11 10:17:15 +0900

    CRUD API追加してみた

--- main.go
+++ main.go
@@ -1,11 +1,69 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 )
 
+/*
+	/todo/{id}
+*/
+func getTodoById(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("詳細取得")
+	fmt.Fprintf(w, "詳細取得")
+}
+func editTodoById(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("詳細取得")
+	fmt.Fprintf(w, "詳細取得")
+}
+func deleteTodoById(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("詳細取得")
+	fmt.Fprintf(w, "詳細取得")
+}
+/*
+	/todos
+*/
+// TODO: リクエストに応じて処理をハンドリングする
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		fmt.Fprintf(w, "HTTPメソッドはGETにしてね!!雑魚!!")
+		return
+	}
+	fmt.Printf("全件取得")
+	fmt.Fprintf(w, "全件取得")
+}
+/*
+	/todo
+*/
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		fmt.Fprintf(w, "HTTPメソッドはPOSTにしてね!!雑魚!!")
+		return
+	}
+	fmt.Printf("作成")
+	fmt.Fprintf(w, "作成")
+}
+
+// IDによって処理を分けるためのハンドラー
+func todoByIdRequestHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+		case "GET":
+			getTodoById(w, r)
+		case "DELETE":
+			deleteTodoById(w, r)
+		case "PUT":
+			editTodoById(w, r)
+		default:
+				http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
-	fmt.Printf("Starting server at port 8080\n")
+
+	http.HandleFunc("/todo", createTodo)
+ 	http.HandleFunc("/todos", getTodos)
+ 	http.HandleFunc("/todo/{id}", todoByIdRequestHandler)
+
+	fmt.Printf("Starting server at port 8000\n")
 	http.ListenAndServe(":8000", nil)
 }
